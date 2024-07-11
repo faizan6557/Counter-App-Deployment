0e21b4bb2e55c59308403abda760e9cbe8d25e7d
Faizan <160043336+faizan6557@users.noreply.github.com>
2024-07-11 23:38:36 +0500

    Update node_group.tf

--- terraform/node_group.tf
+++ terraform/node_group.tf
@@ -5,8 +5,8 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = aws_subnet.eks_subnet[*].id
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 1
+    max_size     = 1
     min_size     = 1
   }
 
